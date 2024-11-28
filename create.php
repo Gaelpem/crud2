20fcdfac46db020c348b92ead1ee14f8304028d4
Gaelpem <pembelegael@gmail.com>
2024-11-28 12:06:30 +0100

    essai

--- create.php
+++ create.php
@@ -3,7 +3,7 @@
  const DB_NAME = 'tache'; 
  const DB_USER = 'root'; 
  const DB_PASS = 'root'; 
- 
+ effefefffef
 $dsn = "mysql:host=". DB_HOST . ";dbname=" .DB_NAME ; 
 
 //DSN signifie "Data Source Name" (Nom de la source de données). C'est une chaîne de texte qui permet à votre application PHP de se connecter à une base de données.
