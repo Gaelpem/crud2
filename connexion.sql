6f3a80524239f118109dd815a4b702b195bd8bdb
Gaelpem <pembelegael@gmail.com>
2024-12-08 17:11:06 +0100

    session6

---
+++ connexion.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:8889
+-- Généré le : dim. 08 déc. 2024 à 15:55
+-- Version du serveur : 8.0.35
+-- Version de PHP : 8.2.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `formulaire_contact`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `connexion`
+--
+
+CREATE TABLE `connexion` (
+  `id` int NOT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `connexion`
+--
+ALTER TABLE `connexion`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `@` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `connexion`
+--
+ALTER TABLE `connexion`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- inscription.php
+++ inscription.php
@@ -1,6 +1,118 @@
 <?php
-session_start(); 
-require_once"config.php"; 
+include 'session.php';
+check_session(); //on vérifie si la personne a le droit d'accéder à cette page
+require 'config.php'; //accès à la base de données
+
+// si la personne est deja connectée elle sera dirigé à la page index.php; 
+if(isset($_SESSION["user_name"])){
+    header("Location: index.php"); 
+    exit; 
+
+    //connexion de la base de donne 
+    $sql = "SELECT * FROM connexion "; 
+    $stmt = $pdo->prepare($sql); 
+     $stmt->execute();
+}
+class Utilisateur{
+    const ERROR_NOM = "Nom incorrect";
+    const ERROR_PRENOM = "Prenom incorrect";  
+    const ERROR_EMAIL = "Email  incorrect"; 
+    const ERROR_MDP = "Mot de passe  incorrect"; 
+
+    
+    private string $nom =""; 
+    private string $prenom =""; 
+    private string $email =""; 
+    private string $mdp = ""; 
+
+    public function __construct(string $nom, string $prenom, string $email, string $mdp){
+        if($nom && $prenom && $email &&  $mdp){
+            $this->setNom($nom);
+            $this->setPrenom($prenom);  
+            $this->setEmail($email); 
+            $this->setMdp($mdp); 
+        }
+    }
+
+    public function setNom($nom)
+    {
+          $nom = strtolower($nom); 
+            if(ctype_alpha($nom)){
+                if(iconv_strlen($nom)>= 3 && iconv_strlen($nom)<= 20){
+                    $this->nom = $nom; 
+                }else{
+                    echo sel::ERROR_NOM ; 
+                }
+            }else{
+                echo sel::ERROR_NOM ; 
+            }
+          }
+
+
+          public function setPrenom($prenom)
+    {
+          $prenom = strtolower($prenom); 
+            if(ctype_alpha($nom)){
+                if(iconv_strlen($prenom)>= 3 && iconv_strlen($prenom)<= 20){
+                    $this->prenom = $prenom; 
+                }else{
+                    echo sel::ERROR_PRENOM; 
+                }
+            }else{
+                echo sel::ERROR_PRENOM ; 
+            }
+          }
+
+
+          public function setEmail($email){
+               $email = strtolower($email);
+               if(filter_var($email,FILTER_DEFAULT)){
+                $this->email = $email;
+               }else{
+                echo sef::ERROR_EMAIL; 
+               }
+          }
+          
+          public function setMdp($mdp){
+            $mdp= strtolower($mdp);
+            $caractereSpeciaux = "!?:%*$"; 
+            $contientCaractereSpecial = false;
+            //on decoupe chaque element pour les transformer en tableaux 
+            foreach(str_split($caractereSpeciaux) as $caractere){
+                //on verifie si le mdp contient au moins un caractere special 
+                if(strpos($mdp, ) !== false){
+                    $contientCaractereSpecial = true;
+                break; // On peut arrêter la boucle dès qu'on trouve un caractère spécial
+                }
+            }
+
+            $this->mdp = $mdp;
+       }
+     
+       public function getNom(){
+        return $this->nom ; 
+       }
+       public function getPrenom(){
+        return $this->prenom; 
+       }
+       public function getEmail(){
+        return $this->email ; 
+       }
+       public function getMdp(){
+        return $this->mdp; 
+       }
+
+
+    }
+
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+         
+        if (isset($_POST["user_name"], $_POST["user_prenom"], $_POST["user_email"], $_POST["user_mdp"])) {
+            // Code à exécuter si les données POST existent
+        }
+        
+    }
 ?>
 
 
